60ec7d6c768d28c845659442070c641fa6ae8a04
mdini <michaeldini@gmail.com>
2025-06-06 20:23:44 -0400

    refactor: remove HTTPS configuration from Vite server settings

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { configDefaults } from 'vitest/config'
-import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: {
-      key: fs.readFileSync('certs/key.pem'),
-      cert: fs.readFileSync('certs/cert.pem'),
-    },
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
