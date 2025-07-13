157231c741865be21e57805a85c5e72c2c0f5159
mdini <michaeldini@gmail.com>
2025-07-13 18:28:01 -0400

    refactor: restore MetatextReview types and remove temp_MetatextReview types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
 export * from './documents';
-export * from './temp_MetatextReview.types';
+export * from './MetatextReview.types';
 export * from './error';
 export { getErrorMessage } from './error';
