a61a41b197e3afec8774a272a4bd1f4eaafa6148
mdini <michaeldini@gmail.com>
2025-07-13 17:54:13 -0400

    refactor: correct import path for MetatextCreateForm to align with naming convention

--- src/pages/MetaText/MetaTextPage.tsx
+++ src/pages/MetaText/MetaTextPage.tsx
@@ -13,7 +13,7 @@ import {
 } from 'components';
 import { FADE_IN_DURATION } from 'constants';
 
-import MetatextCreateForm from './components/MetaTextCreateForm';
+import MetatextCreateForm from './components/MetatextCreateForm';
 
 function MetatextPage(): ReactElement {
 
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,4 @@
 export * from './documents';
 export * from './MetatextReview.types';
 export * from './error';
-export * from './MetatextReview.types';
 export { getErrorMessage } from './error';
